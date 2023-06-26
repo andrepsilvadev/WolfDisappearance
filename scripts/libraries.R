aa46cb40ae3f280dc585eb8a076c23f068dcce3c
andrepsilvadev <pintodasilva.a@gmail.com>
2023-06-26 13:44:12 +0000

    update

--- scripts/libraries.R
+++
@@ -1,8 +0,0 @@
-library(easypackages)
-easypackages::packages("credentials","tidyverse","reshape2", "slider", "gridExtra",
-                       "sf", "viridis",
-                       "lme4", "DHARMa", "performance", "AICcmodavg")
-  
-#Current token
-#credentials::set_github_pat("TOKEN")
-#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
